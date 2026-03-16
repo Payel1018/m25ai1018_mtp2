7421841161048cda92357f527b698b35b63c67a2
Payel1018 <m25ai1018@iitj.ac.in>
2026-03-16 23:43:37 +0530

    updated

--- data_processing.py
+++ data_processing.py
@@ -37,7 +37,7 @@ class DataPreprocess:
         self.df['mental_health_spt'] = (
             self.df['mental_health_support']
             .str.lower()
-            .map({'yes': 1, 'no': 0})
+            .map({'yes': 0, 'no': 1})
         )
 
         self.df['exercise'] = (
@@ -52,35 +52,46 @@ class DataPreprocess:
             .map({'low': 0, 'high': 1, 'moderate': 2})
         )
 
+        self.df['sleep quality'] = (
+            self.df['sleep_quality']
+            .str.lower()
+            .map({'good': 1, 'poor': 0, 'excellent': 2, 'fair': 1})
+        )
+
         print("Categorical encoding completed.")
-## All assumptions made here are based on Medical referrance to Max Normal value an individual can have to be considerred in range
+
     def create_disease_labels(self):
 
-        # High Insulin Flag (Assuming fasting insulin in µIU/mL)
+        # High Insulin Flag (Assuming fasting insulin)
         self.df['High_Insulin'] = np.where(
             self.df['insulin'] > 25, 1, 0
         )
         # Diabetes
         self.df['Diabetes'] = np.where(
-            (self.df['glucose'] >= 126) |
-            ((self.df['bmi'] >= 30) & (self.df['sugar_intake'] > 50)) |
+            ((self.df['glucose'] >= 126) & (self.df['sugar_intake'] > 50)) |
+              ((self.df['bmi'] >= 30) & (self.df['sugar_intake'] > 50)) |
               ( (self.df['High_Insulin'] == 1)),
             1, 0
         )
 
         # Hypertension
         self.df['Hypertension'] = np.where(
-            (self.df['blood_pressure'] >= 140) |
-            ((self.df['bmi'] >= 30) & (self.df['stress_level'] > 7)),
+            (self.df['blood_pressure'] >= 140) & (self.df['age'] >= 35)|
+            ((self.df['bmi'] >= 30) & (self.df['stress_level'] > 6) &
+             (self.df['age'] >= 35)),
             1, 0
         )
 
         # Cardiovascular
         self.df['Cardiovascular'] = np.where(
             (self.df['cholesterol'] >= 240) |
-            (self.df['smoking'] == 2) |
-            (self.df['Hypertension'] == 1) |
-            (self.df['Diabetes'] == 1),
+            ((self.df['smoking'] == 2) &
+            (self.df['Hypertension'] == 1) &
+            (self.df['Diabetes'] == 1))  |
+            ((self.df['stress_level'] > 6) &
+            (self.df['smoking'] == 2) &
+            (self.df['Hypertension'] == 1) &
+             (self.df['heart_rate'] > 90))    ,
             1, 0
         )
 
@@ -99,7 +110,8 @@ class DataPreprocess:
         self.df['Healthy'] = np.where(
             (self.df['Diabetes'] == 0) &
             (self.df['Hypertension'] == 0) &
-            (self.df['Cardiovascular'] == 0),
+            (self.df['Cardiovascular'] == 0) &
+            (self.df['Obesity_Class'] < 2),
             1, 0
         )
 
@@ -112,13 +124,22 @@ class DataPreprocess:
 # Execute the code and create a new Master file with proper data
 processor = DataPreprocess("Master dataset.csv")
 
-processor.handle_missing_values()
+
 processor.encode_categorical()
 processor.create_disease_labels()
+processor.handle_missing_values()
 
 df_processed = processor.get_processed_data()
 
 df_processed.head()
+
+df_processed = df_processed.drop(columns=["occupation", "insurance", "device_usage",
+                      "bmi_estimated", "bmi_scaled", "bmi_corrected",
+                      "waist_size", "sleep_quality", "work_hours",
+                      "education_level", "job_type", "occupation", "diet_type",
+                      "target", "electrolyte_level", "gene_marker_flag",
+                      "environmental_risk_score", "daily_supplement_dosage"
+                      ])
 
 df_processed.to_csv("Master_dataset_with_labels.csv", index=False)
 print("New dataset saved successfully.")
